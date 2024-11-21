03a6fb13ca01a8a5ea24f50d721112c3586e3242
KlajdiMexhaj <mexhajklajdi@gmail.com>
2024-11-21 13:52:56 +0100

    Commit APPchange2

--- djathtas1912/settings.py
+++ djathtas1912/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^h(**ze4-#_d14=&x7hlp0t1&rkpxa31*%fi0(()6b&93#k1b+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['djathtas1912.pythonanywhere.com','127.0.0.1','localhost']
 
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'djathtasApp',
     'django_ckeditor_5',
-    'defender',
+    
 ]
 
 MIDDLEWARE = [
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    'defender.middleware.FailedLoginMiddleware',
+    
 
 ]
 
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 
-DEFENDER_REDIS_URL = 'redis://localhost:6379/0'
+
 
 
 
--- djathtas1912/urls.py
+++ djathtas1912/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
 
-    path('djathtas1912-admin/defender/', include('defender.urls')),
+    # path('djathtas1912-admin/defender/', include('defender.urls')),
     path('djathtas1912-admin/', admin.site.urls),
     path('',include("djathtasApp.urls")),
 ]
